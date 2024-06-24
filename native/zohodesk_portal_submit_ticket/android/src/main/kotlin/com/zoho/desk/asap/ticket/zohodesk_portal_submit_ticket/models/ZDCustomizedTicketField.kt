bc7e1a6d6c7486f9e7adbd512fa1b5a9ee57619e
prakash-15296 <prakash.vel@zohocorp.com>
2024-06-24 12:00:45 +0530

    Added package name in kt files

--- native/zohodesk_portal_submit_ticket/android/src/main/kotlin/com/zoho/desk/asap/ticket/zohodesk_portal_submit_ticket/models/ZDCustomizedTicketField.kt
+++ native/zohodesk_portal_submit_ticket/android/src/main/kotlin/com/zoho/desk/asap/ticket/zohodesk_portal_submit_ticket/models/ZDCustomizedTicketField.kt
@@ -1,3 +1,5 @@
+package com.zoho.desk.asap.ticket.zohodesk_portal_submit_ticket.models
+
 data class ZDCustomizedTicketField (
     val fieldName: String,
     val value: String,
--- native/zohodesk_portal_submit_ticket/android/src/main/kotlin/com/zoho/desk/asap/ticket/zohodesk_portal_submit_ticket/models/ZDCustomizedTicketForm.kt
+++ native/zohodesk_portal_submit_ticket/android/src/main/kotlin/com/zoho/desk/asap/ticket/zohodesk_portal_submit_ticket/models/ZDCustomizedTicketForm.kt
@@ -1,3 +1,5 @@
+package com.zoho.desk.asap.ticket.zohodesk_portal_submit_ticket.models
+
 data class ZDCustomizedTicketForm(
     val departmentId: String,
     val layoutId: String,

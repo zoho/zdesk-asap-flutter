693ffdfc5d3a47b3367712f1405ea065cb3a599a
prakash-15296 <prakash.vel@zohocorp.com>
2025-02-26 12:16:43 +0530

    Added SalesIQ Configuration

--- native/zohodesk_portal_siq/android/src/main/kotlin/com/zoho/desk/asap/siq/zohodesk_portal_siq/ZohodeskPortalSiqPlugin.kt
+++ native/zohodesk_portal_siq/android/src/main/kotlin/com/zoho/desk/asap/siq/zohodesk_portal_siq/ZohodeskPortalSiqPlugin.kt
@@ -8,6 +8,9 @@ import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import com.zoho.desk.asap.siq.ZohoDeskPortalSalesIQ
+import com.zoho.desk.asap.siq.Component
+import com.zoho.desk.asap.siq.LauncherMode
+import com.zoho.desk.asap.siq.SalesIQInitCallback
 
 /** ZohodeskPortalSiqPlugin */
 class ZohodeskPortalSiqPlugin: FlutterPlugin, MethodChannel.MethodCallHandler, ActivityAware {
@@ -28,6 +31,19 @@ class ZohodeskPortalSiqPlugin: FlutterPlugin, MethodChannel.MethodCallHandler, A
       "show" -> activity?.let {
         ZohoDeskPortalSalesIQ.show(activity)
       } ?: kotlin.run { result.notImplemented() }
+      "setSalesIQInitCallback" -> setSalesIQInitCallback(call, result)
+      "setChatBrandDetails" -> setChatBrandDetails(call, result)
+      "setChatVisibility" -> setChatVisibility(call, result)
+      "showOfflineMessage" -> showOfflineMessage(call, result)
+      "allowChatInOfflineMode" -> allowChatInOfflineMode(call, result)
+      "hideQueueTime" -> hideQueueTime(call, result)
+      "showFeedbackAfterSkip" -> showFeedbackAfterSkip(call, result)
+      "enableDragToDismiss" -> enableDragToDismiss(call, result)
+      "setKnowledgeBaseVisibility" -> setKnowledgeBaseVisibility(call, result)
+      "setLoggerEnabled" -> setLoggerEnabled(call, result)
+      "setConversationVisibility" -> setConversationVisibility(call, result)
+      "setConversationTitle" -> setConversationTitle(call, result)
+      "setLauncherVisibility" -> setLauncherVisibility(call, result)
       else -> result.notImplemented()
     }
   }
@@ -48,5 +64,95 @@ class ZohodeskPortalSiqPlugin: FlutterPlugin, MethodChannel.MethodCallHandler, A
 
   override fun onDetachedFromActivity() {
     activity = null
+  }
+
+  private fun setSalesIQInitCallback(call: MethodCall, result: MethodChannel.Result) {
+    ZohoDeskPortalSalesIQ.setSalesIQInitCallback(object: SalesIQInitCallback{
+      override fun onInitialized() {
+        result.success(true)
+      }
+
+      override fun onException(errorCode: Int, errorMessage: String?) {
+        result.success(errorMessage)
+      }
+    })
+  }
+
+  private fun setChatBrandDetails(call: MethodCall, result: MethodChannel.Result) {
+    val paramsMap = call.arguments as? HashMap<String, Any>
+    val appKey = paramsMap?.get("appKey") as? String?
+    val accessKey = paramsMap?.get("accessKey") as? String?
+
+    ZohoDeskPortalSalesIQ.setChatBrandDetails(activity, appKey, accessKey)
+  }
+
+  private fun setChatVisibility(call: MethodCall, result: MethodChannel.Result) {
+    val paramsMap = call.arguments as? HashMap<String, Any>
+    val component = paramsMap?.get("component") as? String?
+    val isVisible = paramsMap?.get("visibility") as? Boolean?
+    if(component != null && isVisible != null){
+      ZohoDeskPortalSalesIQ.setChatVisibility(Component.valueOf(component), isVisible)
+    }
+  }
+
+  private fun showOfflineMessage(call: MethodCall, result: MethodChannel.Result) {
+    (call.arguments as? Boolean?)?.let { isShow ->
+      ZohoDeskPortalSalesIQ.showOfflineMessage(isShow)
+    }
+  }
+
+  private fun allowChatInOfflineMode(call: MethodCall, result: MethodChannel.Result) {
+    (call.arguments as? Boolean?)?.let { isAllow ->
+      ZohoDeskPortalSalesIQ.allowChatInOfflineMode(isAllow)
+    }
+  }
+
+  private fun hideQueueTime(call: MethodCall, result: MethodChannel.Result) {
+    (call.arguments as? Boolean?)?.let { isAllow ->
+      ZohoDeskPortalSalesIQ.hideQueueTime(isAllow)
+      result.success(true)
+    }
+  }
+
+  private fun showFeedbackAfterSkip(call: MethodCall, result: MethodChannel.Result) {
+    (call.arguments as? Boolean?)?.let { isShow ->
+      ZohoDeskPortalSalesIQ.showFeedbackAfterSkip(isShow)
+    }
+  }
+
+  private fun enableDragToDismiss(call: MethodCall, result: MethodChannel.Result) {
+    (call.arguments as? Boolean?)?.let { isEnable ->
+      ZohoDeskPortalSalesIQ.enableDragToDismiss(isEnable)
+    }
+  }
+
+  private fun setKnowledgeBaseVisibility(call: MethodCall, result: MethodChannel.Result) {
+    (call.arguments as? Boolean?)?.let { visibility ->
+      ZohoDeskPortalSalesIQ.setKnowledgeBaseVisibility(visibility)
+    }
+  }
+
+  private fun setLoggerEnabled(call: MethodCall, result: MethodChannel.Result) {
+    (call.arguments as? Boolean?)?.let { isEnable ->
+      ZohoDeskPortalSalesIQ.setLoggerEnabled(isEnable)
+    }
+  }
+
+  private fun setConversationVisibility(call: MethodCall, result: MethodChannel.Result) {
+    (call.arguments as? Boolean?)?.let { visibility ->
+      ZohoDeskPortalSalesIQ.setConversationVisibility(visibility)
+    }
+  }
+
+  private fun setConversationTitle(call: MethodCall, result: MethodChannel.Result) {
+    (call.arguments as? String?)?.let { title ->
+      ZohoDeskPortalSalesIQ.setConversationTitle(title)
+    }
+  }
+
+  private fun setLauncherVisibility(call: MethodCall, result: MethodChannel.Result) {
+    (call.arguments as? String?)?.let { visibility ->
+      ZohoDeskPortalSalesIQ.setLauncherVisibility(LauncherMode.valueOf(visibility))
+    }
   }
 }
--- native/zohodesk_portal_siq/lib/zohodesk_portal_siq_method_channel.dart
+++ native/zohodesk_portal_siq/lib/zohodesk_portal_siq_method_channel.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
+import 'common/ChatComponent.dart';
+import 'common/LauncherMode.dart';
+import 'common/SalesIQInitCallback.dart';
 import 'zohodesk_portal_siq_platform_interface.dart';
 
 /// An implementation of [ZohodeskPortalSiqPlatform] that uses method channels.
@@ -12,4 +15,53 @@ class MethodChannelZohodeskPortalSiq extends ZohodeskPortalSiqPlatform {
   ///ASAP Chat show channel method
   @override
   Future<void> show() async => await methodChannel.invokeMethod('show');
+
+  @override
+  Future<void> setSalesIQInitCallback(SalesIQInitCallback callback) async {
+    dynamic response = await methodChannel.invokeMethod('setSalesIQInitCallback');
+    if(response is String){
+      callback.onException(response);
+    }else{
+      callback.onInitialized();
+
+    }
+
+  }
+
+  @override
+  Future<void> setChatBrandDetails(String appKey, String accessKey) async => await methodChannel.invokeMethod('setChatBrandDetails', {'appKey': appKey, 'accessKey': accessKey});
+
+  @override
+  Future<void> setChatVisibility(ChatComponent component, bool visibility) async => await methodChannel.invokeMethod('setChatVisibility', {'component': component.name, 'visibility': visibility});
+
+  @override
+  Future<void> showOfflineMessage(bool isShow) async => await methodChannel.invokeMethod('showOfflineMessage', isShow);
+
+  @override
+  Future<void> allowChatInOfflineMode(bool isAllow) async => await methodChannel.invokeMethod('allowChatInOfflineMode', isAllow);
+
+  @override
+  Future<void> hideQueueTime(bool isHide) async => await methodChannel.invokeMethod('hideQueueTime', isHide);
+
+  @override
+  Future<void> showFeedbackAfterSkip(bool isShow) async => await methodChannel.invokeMethod('showFeedbackAfterSkip', isShow);
+
+  @override
+  Future<void> enableDragToDismiss(bool isEnable) async => await methodChannel.invokeMethod('enableDragToDismiss', isEnable);
+
+  @override
+  Future<void> setKnowledgeBaseVisibility(bool visibility) async => await methodChannel.invokeMethod('setKnowledgeBaseVisibility', visibility);
+
+  @override
+  Future<void> setLoggerEnabled(bool isEnable) async => await methodChannel.invokeMethod('setLoggerEnabled', isEnable);
+
+  @override
+  Future<void> setConversationVisibility(bool visibility) async => await methodChannel.invokeMethod('setConversationVisibility', visibility);
+
+  @override
+  Future<void> setConversationTitle(String title) async => await methodChannel.invokeMethod('setConversationTitle', title);
+
+  @override
+  Future<void> setLauncherVisibility(LauncherMode visibility) async => await methodChannel.invokeMethod('setLauncherVisibility', visibility.name);
+
 }
